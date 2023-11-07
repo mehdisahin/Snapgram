fbcfa620642995b991538e4b6afdd83c56c790c0
Mehdi Şahin <mehdisahin@yorkandchapel.com>
2023-11-07 14:50:10 +0300

    add create user

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
-import './globals.css'
-import { Route, Routes } from 'react-router-dom';
-import SigninForm from './_auth/forms/SigninForm';
-import Home from './_root/pages/Home';
-import SignupForm from './_auth/forms/SignupForm';
-import AuthLayout from './_auth/AuthLayout';
-import RootLayout from './_root/RootLayout';
+import "./globals.css";
+import { Route, Routes } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import Home from "./_root/pages/Home";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import { Toaster } from "@/components/ui/toaster";
+
 const App = () => {
   return (
-<main className="flex h-screen">
-    <Routes>
-    {/*Public Routes */}
-    <Route element= {<AuthLayout/>}>
+    <main className="flex h-screen">
+      <Routes>
+        {/*Public Routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
 
-    <Route path="/sign-in" element={<SigninForm/>} />
-    <Route path="/sign-up" element={<SignupForm/>} />
+        {/*Private Routes */}
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+      <Toaster
+    </main>
+  );
+};
 
-    </Route>
-
-    {/*Private Routes */}
-    <Route path="/" element={<RootLayout/>}>
-
-    <Route index element={<Home/>}/>
-
-    </Route>
-   
-    </Routes>
-
-</main>
-  )
-}
-
-export default App
+export default App;
--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,8 +1,9 @@
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useToast } from "@/components/ui/use-toast";
+
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -15,8 +16,10 @@ import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
 import Loader from "@/components/shared/Loader";
 import { Link } from "react-router-dom";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
+  const { toast } = useToast();
   const isLoading = false;
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
@@ -31,9 +34,13 @@ const SignupForm = () => {
 
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
-    const newUser = await;
-    console.log(values);
-  }
+    const newUser = await createUserAccount(values);
+    if (!newUser) {
+      return toast({
+        title: "Sign up failed, Please try again.",
+      });
+    }
+    // const session = await signInAccount()
 
   return (
     <Form {...form}>
